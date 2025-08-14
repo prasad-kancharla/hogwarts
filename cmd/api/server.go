05eca5c0e47eb79f88c4060b31e8e1d20cb1675e
prasad-kancharla <kancharlaguruprasad@gmail.com>
2025-08-14 12:13:02 +0530

    Organized handlers

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -14,82 +14,128 @@ type User struct {
 	City string `json:"city"`
 }
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Got a request to home page")
+	w.Write([]byte("Hello Homepage"))
+}
+
+func studentsHandler(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case http.MethodGet:
+		log.Println("Got a request to students using method " + r.Method)
+		w.Write([]byte("Hello students from " + r.Method))
+	case http.MethodPost:
+
+		// processing x-www-form-urlencoded
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Error parsing data", http.StatusBadRequest)
+		}
+		fmt.Println(r.Form)
+		processedData := make(map[string]interface{})
+
+		for k, v := range r.Form {
+			processedData[k] = v[0]
+		}
+
+		fmt.Println(processedData)
+
+		// processing raw json
+		byteBody, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error in processing raw json", http.StatusBadRequest)
+		}
+
+		fmt.Println(string(byteBody))
+
+		var user User
+		err = json.Unmarshal(byteBody, &user)
+		if err != nil {
+			http.Error(w, "Error converting json body to go struct", http.StatusBadRequest)
+			return
+		}
+		fmt.Println(user)
+
+		defer r.Body.Close()
+
+		log.Println("Got a request to students using method " + r.Method)
+		w.Write([]byte("Hello students from " + r.Method))
+	case http.MethodPut:
+		log.Println("Got a request to students using method " + r.Method)
+		w.Write([]byte("Hello students from " + r.Method))
+	case http.MethodPatch:
+		log.Println("Got a request to students using method " + r.Method)
+		w.Write([]byte("Hello students from " + r.Method))
+	case http.MethodDelete:
+		log.Println("Got a request to students using method " + r.Method)
+		w.Write([]byte("Hello students from " + r.Method))
+	default:
+		log.Println("Got a request to students from unsupported method:", r.Method)
+		w.Write([]byte("Hello students from " + r.Method))
+	}
+
+}
+
+func teachersHandler(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case http.MethodGet:
+		log.Println("Got a request to teachers using method " + r.Method)
+		w.Write([]byte("Hello teachers from " + r.Method))
+	case http.MethodPost:
+		log.Println("Got a request to teachers using method " + r.Method)
+		w.Write([]byte("Hello teachers from " + r.Method))
+	case http.MethodPut:
+		log.Println("Got a request to teachers using method " + r.Method)
+		w.Write([]byte("Hello teachers from " + r.Method))
+	case http.MethodPatch:
+		log.Println("Got a request to teachers using method " + r.Method)
+		w.Write([]byte("Hello teachers from " + r.Method))
+	case http.MethodDelete:
+		log.Println("Got a request to teachers using method " + r.Method)
+		w.Write([]byte("Hello teachers from " + r.Method))
+	default:
+		log.Println("Got a request to teachers from unsupported method:", r.Method)
+		w.Write([]byte("Hello teachers from " + r.Method))
+	}
+}
+
+func execsHandler(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case http.MethodGet:
+		log.Println("Got a request to execs using method " + r.Method)
+		w.Write([]byte("Hello execs from " + r.Method))
+	case http.MethodPost:
+		log.Println("Got a request to execs using method " + r.Method)
+		w.Write([]byte("Hello execs from " + r.Method))
+	case http.MethodPut:
+		log.Println("Got a request to execs using method " + r.Method)
+		w.Write([]byte("Hello execs from " + r.Method))
+	case http.MethodPatch:
+		log.Println("Got a request to execs using method " + r.Method)
+		w.Write([]byte("Hello execs from " + r.Method))
+	case http.MethodDelete:
+		log.Println("Got a request to execs using method " + r.Method)
+		w.Write([]byte("Hello execs from " + r.Method))
+	default:
+		log.Println("Got a request to execs from unsupported method:", r.Method)
+		w.Write([]byte("Hello execs from " + r.Method))
+	}
+}
+
 func main() {
 	port := ":3001"
 	fmt.Println("Starting server at port", port)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Got a request to home page")
-		w.Write([]byte("Hello Homepage"))
-	})
+	http.HandleFunc("/", rootHandler)
 
-	http.HandleFunc("/students", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/students", studentsHandler)
 
-		switch r.Method {
-		case http.MethodGet:
-			log.Println("Got a request to students using method " + r.Method)
-			w.Write([]byte("Hello students from " + r.Method))
-		case http.MethodPost:
+	http.HandleFunc("/teachers", teachersHandler)
 
-			// processing x-www-form-urlencoded
-			err := r.ParseForm()
-			if err != nil {
-				http.Error(w, "Error parsing data", http.StatusBadRequest)
-			}
-			fmt.Println(r.Form)
-			processedData := make(map[string]interface{})
-
-			for k, v := range r.Form {
-				processedData[k] = v[0]
-			}
-
-			fmt.Println(processedData)
-
-			// processing raw json
-			byteBody, err := io.ReadAll(r.Body)
-			if err != nil {
-				http.Error(w, "Error in processing raw json", http.StatusBadRequest)
-			}
-
-			fmt.Println(string(byteBody))
-
-			var user User
-			err = json.Unmarshal(byteBody, &user)
-			if err != nil {
-				http.Error(w, "Error converting json body to go struct", http.StatusBadRequest)
-				return
-			}
-			fmt.Println(user)
-
-			defer r.Body.Close()
-
-			log.Println("Got a request to students using method " + r.Method)
-			w.Write([]byte("Hello students from " + r.Method))
-		case http.MethodPut:
-			log.Println("Got a request to students using method " + r.Method)
-			w.Write([]byte("Hello students from " + r.Method))
-		case http.MethodPatch:
-			log.Println("Got a request to students using method " + r.Method)
-			w.Write([]byte("Hello students from " + r.Method))
-		case http.MethodDelete:
-			log.Println("Got a request to students using method " + r.Method)
-			w.Write([]byte("Hello students from " + r.Method))
-		default:
-			log.Println("Got a request to students from unsupported method:", r.Method)
-			w.Write([]byte("Hello students from " + r.Method))
-		}
-
-	})
-
-	http.HandleFunc("/teachers", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Got a request to teachers")
-		w.Write([]byte("Hello teachers"))
-	})
-
-	http.HandleFunc("/execs", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Got a request to execs")
-		w.Write([]byte("Hello execs"))
-	})
+	http.HandleFunc("/execs", execsHandler)
 
 	err := http.ListenAndServe(port, nil)
 	if err != nil {
